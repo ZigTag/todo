a50b3a1d38c13f7bf917c8441f4cff3f6171b8b8
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-08-16 12:37:36 -0700

    Fixed gitignore ignoring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+//TODO: Please document program
 use grep::matcher::Matcher;
 use grep::regex::RegexMatcher;
 use grep::searcher::sinks::UTF8;
@@ -5,9 +6,7 @@ use grep::searcher::{BinaryDetection, SearcherBuilder};
 
 use std::path::Path;
 
-use walkdir::WalkDir;
-
-use git2::Repository;
+use ignore::Walk;
 
 fn main() {
     let matcher = RegexMatcher::new(r".*TODO:.*").unwrap();
@@ -15,56 +14,41 @@ fn main() {
         .binary_detection(BinaryDetection::quit(b'\x00'))
         .build();
     let path = Path::new("./");
-    let mut matches: Vec<(u64, String)> = vec![];
+    let mut matches: Vec<(u64, String, String)> = vec![];
 
-    let _is_git: bool;
-
-    let git = match Repository::open(path) {
-        Ok(repo) => Some(repo),
-        Err(_) => None,
-    };
-
-    match git {
-        Some(git) => {
-            _is_git = true;
-
-            for path in path {
-                //TODO: Gitignore is not working properly
-
-                for result in WalkDir::new(path).into_iter().filter(|e| {
-                    git.status_should_ignore(e.as_ref().unwrap().path())
-                        .unwrap()
-                }) {
-                    let dent = match result {
-                        Ok(dent) => dent,
-                        Err(err) => {
-                            eprintln!("{}", err);
-                            continue;
-                        }
-                    };
-                    if !dent.file_type().is_file() {
-                        continue;
-                    }
-                    println!("after '{}'", dent.path().display());
-                    let result = searcher.search_path(
-                        &matcher,
-                        dent.path(),
-                        UTF8(|line_num, string| {
-                            let my_match = matcher.find(string.as_bytes())?.unwrap();
-                            matches.push((line_num, string[my_match].trim().to_string()));
-                            Ok(true)
-                        }),
-                    );
-                    if let Err(err) = result {
-                        eprintln!("{}: {}", dent.path().display(), err);
-                    }
+    for path in path {
+        for result in Walk::new(path) {
+            let dent = match result {
+                Ok(dent) => dent,
+                Err(err) => {
+                    eprintln!("{}", err);
+                    continue;
                 }
+            };
+            if !dent.path().is_file() {
+                continue;
             }
-        }
-        None => {
-            _is_git = false;
+            println!("dir '{}'", dent.path().display());
+            let result = searcher.search_path(
+                &matcher,
+                dent.path(),
+                UTF8(|line_num, string| {
+                    let my_match = matcher.find(string.as_bytes())?.unwrap();
+                    matches.push((
+                        line_num,
+                        string[my_match].trim().to_string(),
+                        dent.path().display().to_string(),
+                    ));
+                    Ok(true)
+                }),
+            );
+            if let Err(err) = result {
+                eprintln!("{}: {}", dent.path().display(), err);
+            }
         }
     }
 
-    println!("{:?}", matches);
+    for (line, text, path) in matches {
+        println!("\nFile: '{}'\nLine: {}\nText: '{}'", path, line, text);
+    }
 }
