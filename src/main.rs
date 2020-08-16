b2f29f3b3331a19dacb7abf073140d04f3fb7a15
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-08-16 01:45:57 -0700

    Initial Commit
    Commit is broken

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use grep::regex::RegexMatcher;
+use grep::searcher::{SearcherBuilder, BinaryDetection};
+use std::path::Path;
+use grep::searcher::sinks::UTF8;
+use grep::matcher::Matcher;
+use walkdir::WalkDir;
+use git2::Repository;
+
+fn main() {
+    let matcher = RegexMatcher::new(r"TODO:").unwrap();
+    let mut searcher = SearcherBuilder::new()
+        .binary_detection(BinaryDetection::quit(b'\x00'))
+        .build();
+    let path = Path::new("./");
+    let mut matches: Vec<(u64, String)> = vec![];
+
+    let _is_git: bool;
+
+    let git = match Repository::open(path) {
+        Ok(repo) => Some(repo),
+        Err(_) => None,
+    };
+
+    match git {
+        Some(git) => {
+            _is_git = true;
+
+            for path in path {
+                for result in WalkDir::new(path).into_iter().filter(|e| {
+                    !git.status_should_ignore(e.as_ref().unwrap().path()).unwrap()
+                }) {
+                    let dent = match result {
+                        Ok(dent) => dent,
+                        Err(err) => {
+                            eprintln!("{}", err);
+                            continue;
+                        }
+                    };
+                    if !dent.file_type().is_file() {
+                        continue;
+                    }
+                    println!("after '{}'", dent.path().display());
+                    let result = searcher.search_path(
+                        &matcher,
+                        dent.path(),
+                        UTF8(|line_num, string| {
+                            let my_match = matcher.find(string.as_bytes())?.unwrap();
+                            matches.push((line_num, string[my_match].to_string()));
+                            Ok(true)
+                        })
+                    );
+                    if let Err(err) = result {
+                        eprintln!("{}: {}", dent.path().display(), err);
+                    }
+                }
+            }
+        },
+        None => {
+            _is_git = false;
+        },
+    }
+
+    println!("{:?}", matches);
+}
