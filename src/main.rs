14cf7d97dd4963ab772f9ef9203cafd753244ee9
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-08-16 19:04:21 -0700

    Experiementing with git support

--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,46 @@ use grep::regex::RegexMatcher;
 use grep::searcher::sinks::UTF8;
 use grep::searcher::{BinaryDetection, SearcherBuilder};
 
-use std::path::Path;
 use std::io::Write;
+use std::path::Path;
 
 use ignore::WalkBuilder;
 
-use termcolor::{StandardStream, ColorChoice};
+use termcolor::{ColorChoice, StandardStream};
 
 use clap::{App, Arg};
+
+use git2::Repository;
 
 fn main() {
     let args = App::new("todo")
         .version("0.1.0")
         .author("ZigTag <GitHub>")
         .about("Reads out your current TODOs")
-        .arg(Arg::with_name("path")
-            .short("d")
-            .long("path")
-            .value_name("DIR")
-            .help("Sets the working directory. (optional)")
-            .takes_value(true)
-            .required(false)
-            .default_value("./")
-            .index(1))
-        .arg(Arg::with_name("color")
-            .long("color")
-            .value_name("bool")
-            .help("Display text color.")
-            .default_value("true"))
-        .arg(Arg::with_name("show_hidden")
-            .short("h")
-            .long("show-hidden")
-            .help("Includes hidden files."))
+        .arg(
+            Arg::with_name("path")
+                .short("d")
+                .long("path")
+                .value_name("DIR")
+                .help("Sets the working directory. (optional)")
+                .takes_value(true)
+                .required(false)
+                .default_value("./")
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("color")
+                .long("color")
+                .value_name("bool")
+                .help("Display text color.")
+                .default_value("true"),
+        )
+        .arg(
+            Arg::with_name("show_hidden")
+                .short("h")
+                .long("show-hidden")
+                .help("Includes hidden files."),
+        )
         .get_matches();
 
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
@@ -44,8 +52,15 @@ fn main() {
         .binary_detection(BinaryDetection::quit(b'\x00'))
         .build();
     let path = Path::new(args.value_of("path").unwrap());
-    let mut matches: Vec<(u64, String, String)> = vec![];
+    let mut matches: Vec<(usize, String, String)> = vec![];
     let show_hidden = !args.is_present("show_hidden");
+
+    let is_git: bool;
+
+    let git = match Repository::open(path) {
+        Ok(git) => Some(git),
+        Err(_) => None,
+    };
 
     for path in path {
         for result in WalkBuilder::new(path).hidden(show_hidden).build() {
@@ -66,9 +81,9 @@ fn main() {
                 UTF8(|line_num, string| {
                     let my_match = matcher.find(string.as_bytes())?.unwrap();
                     matches.push((
-                        line_num,
+                        line_num as usize,
                         string[my_match].trim().to_string(),
-                        dent.path().display().to_string(),
+                        dent.path().to_str().unwrap().to_string(),
                     ));
                     Ok(true)
                 }),
@@ -79,7 +94,21 @@ fn main() {
         }
     }
 
-    for (line, text, path) in matches {
-        writeln!(&mut stdout, "\nFile: '{}'\nLine: {}\nText: '{}'", path, line, text).unwrap();
+    if let Some(git) = git {
+        is_git = true;
+
+        for (line, text, path) in matches {
+            let blame = git
+                .blame_file(Path::new(&path).strip_prefix("./").unwrap(), None)
+                .unwrap();
+
+            let hunk = blame.get_line(line as usize).unwrap();
+
+            let commit = hunk.final_commit_id();
+
+            writeln!(&mut stdout, "commit {}", commit).unwrap();
+        }
+    } else {
+        is_git = false;
     }
 }
